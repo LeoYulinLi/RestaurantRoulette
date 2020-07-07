4aad3e885cc3cb0746e56d297f62effe1f113118
amendoza514 <amendoza15@cmc.edu>
2020-07-07 09:17:26 -0700

    actual yelp integration I promise

--- server.js
+++ server.js
@@ -29,16 +29,41 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 
-app.post("/api/fetchYelpRestaurant", (req, res) => {
+app.post("/api/fetchYelpRestaurant", async (req, res) => {
   const { categories, latitude, longitude } = req.body;
-  axios.get( 
-    `https://api.yelp.com/v3/businesses/search?categories=${categories}&latitude=${latitude}&longitude=${longitude}`,
+  const initialOffset = Math.floor(Math.random() * 1000);
+  const result1 = await axios.get( 
+    `https://api.yelp.com/v3/businesses/search?categories=${categories}&latitude=${latitude}&longitude=${longitude}&open_now=true&offset=${initialOffset}&limit=1`,
     // {params: categories, latitude, longitude },
     config
-  ).then(result => {
-    res.json(result.data);
-  }).catch(err=> res.json(err));
-})
+  )
+
+  if (result1.data.businesses.length === 0) {
+    const total = result1.data.total;
+    const offset = Math.floor(Math.random() * total);
+    const result2 = await axios.get( 
+      `https://api.yelp.com/v3/businesses/search?categories=${categories}&latitude=${latitude}&longitude=${longitude}&open_now=true&offset=${offset}&limit=1`,
+      // {params: categories, latitude, longitude },
+      config
+    )
+    res.json(result2.data);
+  } else {
+    res.json(result1.data);
+  }
+
+});
+
+
+app.post("/api/fetchYelpAutoCompletion", async (req, res) => {
+  const { text } = req.body;
+  const thing = await axios.get(
+    `https://api.yelp.com/v3/autocomplete?text=${text}`,
+    config
+  );
+  res.json(thing.data.categories);
+
+
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
