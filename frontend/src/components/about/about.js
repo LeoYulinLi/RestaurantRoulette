148219f37893c911db7d06147ae829b47ed5ce7d
Ryan Tran <ryandtran@hotmail.com>
2020-07-22 10:11:02 -0700

    Begin building component for about page

---
+++ frontend/src/components/about/about.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './about.scss';
+
+function About() {
+  return (
+    <div className="about-member container">
+      <div className="about-member image">
+
+      </div>
+
+      <div className="about-member details">
+
+      </div>
+
+      <div className="about-member links">
+        
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ frontend/src/components/about/about_container.js
@@ -0,0 +1,9 @@
+import About from './about';
+
+function AboutPage() {
+  return (
+    <About/>
+  )
+}
+
+export default AboutPage;
