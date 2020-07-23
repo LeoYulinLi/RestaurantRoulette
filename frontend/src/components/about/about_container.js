0ae573ddd4b7a5973c38e199bfafc95e56999b71
Ryan Tran <ryandtran@hotmail.com>
2020-07-23 00:29:04 -0700

    Edit details of each team member

--- frontend/src/components/about/about_container.js
+++ frontend/src/components/about/about_container.js
@@ -15,7 +15,8 @@ const AboutPage = () => {
           github="https://github.com/rd-tran"
           linkedIn="https://www.linkedin.com/in/rdtran/"
           angelList="https://angel.co/u/ryan-tran-5"
-          details="Ryan is a full stack engineer and graduate of UCSD with a B.S. in Electrical Engineering. He is experienced in Javascript, React, Ruby on Rails, and Python. He is a self proclaimed foodie, and wanted to leverage his skills, as well as the skills of his team members to help indecisive foodies pick a place to eat. Acted as lead front end engineer of this project."
+          details="Serving as the lead front end engineer of the project, Ryan is a full stack engineer and graduate of UCSD. Holding a B.S. in Electrical Engineering, he is experienced in Javascript, React, Ruby on Rails, and Python. As a self proclaimed foodie with an impeccable palate, he leveraged his skills, as well as his team's, to pilot this project. The end result is Restaurant Roulette, the perfect app for indecisive foodies."
+          // As a self proclaimed foodie, he wanted to leverage his skills, as well as the skills of his team members to help indecisive foodies pick a place to eat.
         />
 
         <About
@@ -24,8 +25,8 @@ const AboutPage = () => {
           github="https://github.com/LeoYulinLi"
           linkedIn="https://www.linkedin.com/in/leoyulinli/"
           angelList="https://angel.co/u/leoyulinli"
-          details="A software engineer experienced in React, Ruby on Rails, and Java/Kotlin development. Passionate about solving problems that positively impact every day life. Lead back end engineer of this project."
-        />
+          details="Serving as the lead back end engineer of the project, Leo is a software engineer experienced in React, Ruby on Rails, and Java/Kotlin development. He is passionate about using his skills to solve problems that positively impact every day life. As such, he tapped into his extensive programming knowledge, and played an integral part in building the back end. Utilizing async requests in parallel to fetch data from the API to improve overall load time, is one such example."
+          />
 
         <About
           name="Jingwen (Wendy) Kuang"
@@ -33,7 +34,7 @@ const AboutPage = () => {
           github="https://github.com/Jingwenkuang"
           linkedIn="https://www.linkedin.com/in/jingwen-wendy-kuang-169a1b52/"
           angelList="https://angel.co/u/jingwen-kuang"
-          details="Software engineer with a biochemistry background. Previously, performing and developing new assay conditions, now developing new application based on my fluent in Javascript, React, Ruby on Rails, MongoDB, HTML5 and CSS."
+          details="Wendy is a software engineer with a background in biochemistry. In her previous line of work, she piloted analytic investigations in laboratories. Now, she is focused on developing web based applications through her fluency in Javascript, React, Ruby on Rails, MongoDB, HTML5 and CSS."
         />
 
         <About
@@ -42,7 +43,8 @@ const AboutPage = () => {
           github="https://github.com/amendoza514"
           linkedIn="https://www.linkedin.com/in/alex-mendoza-aa4615b5/"
           angelList="https://angel.co/u/alexander-jordan-mendoza"
-          details="Data analyst turned software developer. Using my experience in tools like JavaScript, Ruby and React to channel both my analytical and creative problem solving"
+          details="Alex is a data analyst turned software developer. He used his experience and knowledge of industry standards such as JavaScript, React, HTML, and CSS to channel his analytical and creative problem solving skills to support the project."
+          // Using my experience in tools like JavaScript, Ruby and React to channel his analytical and creative problem solving skills to support the project.
         />
       </div>
     </section>
